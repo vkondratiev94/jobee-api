7275ae166c9c9317316b49374df5b5c2c3b7ee91
vkondratiev94 <vkondratiev94@gmail.com>
2020-12-17 14:03:38 +0200

    creating basic route

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express()
 // Setting up config.env file variables
 dotenv.config({ path: './config/config.env' })
 
+// Importing all routes
+const jobs = require('./routes/jobs')
+
+app.use('/api/v1', jobs)
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT} in ${process.env.NODE_ENV} mode`)
