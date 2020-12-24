94213dccc967a51e104ee2d5a763ebbc6aad6f8f
vkondratiev94 <vkondratiev94@gmail.com>
2020-12-24 19:43:01 +0200

    validation & mongoose id error

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,3 +1,5 @@
+const ErrorHandler = require('../utils/errorHandler')
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500
 
@@ -14,6 +16,18 @@ module.exports = (err, req, res, next) => {
     let error = {...err}
 
     error.message = err.message
+
+    // Wrong Mongoose Object ID Error
+    if (err.name === 'CastError') {
+      const message = `Resource not found. Invalid ${err.path}`
+      error = new ErrorHandler(message, 404)
+    }
+
+    // Handling Mongoose Validation Error
+    if (err.name === 'ValidationError') {
+      const message = Object.values(err.errors).map(value => value.message)
+      error = new ErrorHandler(message, 400)
+    }
 
     res.status(err.statusCode).json({
       success: false,
