c1fbb75442ae61c81d4a355955bcfa54d0cdb6a6
vkondratiev94 <vkondratiev94@gmail.com>
2020-12-24 18:55:08 +0200

    catching async errors

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,9 +1,10 @@
 const geoCoder = require('../utils/geocoder')
-const Job = require('../models/jobs')
 const ErrorHandler = require('../utils/errorHandler')
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+const Job = require('../models/jobs')
 
 // Get all jobs => /api/v1/jobs
-exports.getJobs = async (req, res, next) => {
+exports.getJobs = catchAsyncErrors(async (req, res, next) => {
   const jobs = await Job.find()
 
   res.status(200).json({
@@ -11,10 +12,10 @@ exports.getJobs = async (req, res, next) => {
     results: jobs.length,
     data: jobs,
   })
-}
+})
 
 // Create a new Job => /api/v1/job/new
-exports.newJob = async (req, res, next) => {
+exports.newJob = catchAsyncErrors(async (req, res, next) => {
   const job = await Job.create(req.body)
 
   res.status(200).json({
@@ -22,10 +23,10 @@ exports.newJob = async (req, res, next) => {
     message: 'Job Created',
     data: job,
   })
-}
+})
 
 // Update a Job => /api/v1/job/:id
-exports.updateJob = async (req, res, next) => {
+exports.updateJob = catchAsyncErrors(async (req, res, next) => {
   let job = await Job.findById(req.params.id)
 
   if (!job) {
@@ -43,10 +44,10 @@ exports.updateJob = async (req, res, next) => {
     message: 'Job is updated',
     data: job,
   })
-}
+})
 
 // Delete a Job => /api/v1/job/:id
-exports.deleteJob = async (req, res, next) => {
+exports.deleteJob = catchAsyncErrors(async (req, res, next) => {
   const job = await Job.findById(req.params.id)
 
   if (!job) {
@@ -62,10 +63,10 @@ exports.deleteJob = async (req, res, next) => {
     success: true,
     message: 'Job is deleted',
   })
-}
+})
 
 // Get a single Job with id and slug => /api/v1/job/:id/:slug
-exports.getJob = async (req, res, next) => {
+exports.getJob = catchAsyncErrors(async (req, res, next) => {
   const job = await Job.find({ $and: [
     { _id: req.params.id },
     { slug: req.params.slug },
@@ -82,10 +83,10 @@ exports.getJob = async (req, res, next) => {
     success: true,
     data: job,
   })
-}
+})
 
 // Search jobs with radius => /api/v1/jobs/:zipcode/:distance
-exports.getJobsInRadius = async (req, res, next) => {
+exports.getJobsInRadius = catchAsyncErrors(async (req, res, next) => {
   const { zipcode, distance } = req.params
 
   // Getting latitude & longitude from geocoder with zipcode
@@ -108,10 +109,10 @@ exports.getJobsInRadius = async (req, res, next) => {
     results: jobs.length,
     data: jobs,
   })
-}
+})
 
 // Get stats about a topic (job) => /api/v1/stats/:topic
-exports.jobStats = async (req, res, next) => {
+exports.jobStats = catchAsyncErrors(async (req, res, next) => {
   const stats = await Job.aggregate([
     {
       $match: {
@@ -143,4 +144,4 @@ exports.jobStats = async (req, res, next) => {
     success: true,
     data: stats,
   })
-}
+})
